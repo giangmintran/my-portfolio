4a5e4e6786821b7a34f4e18295c108740fa1d023
giangmintran <giangcoi2001@gmail.com>
2025-07-02 17:00:03 +0700

    Update About.jsx

--- react-portfolio/src/components/About.jsx
+++ react-portfolio/src/components/About.jsx
@@ -20,7 +20,7 @@ const About = () => (
             <Col xs={24} md={8} style={{ textAlign: 'center' }}>
                 <Image
                     width={200}
-                    src="public/avt.png"
+                    src="/assets/avt.png"
                     alt="Your avatar"
                     style={{ borderRadius: '50%' }}
                     preview={false}
